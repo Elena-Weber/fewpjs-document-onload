dd57b2c537cbd589da8715f25b39269e155efa7e
Elena-Weber <crochetmiracles@gmail.com>
2021-05-25 21:29:57 -0400

    commit tree a7f97e470356467509d21596c1d84d0463ff3421

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    textChange();
+});
+
+function textChange() {
+    document.getElementById('text').innerHTML = "This is really cool!";
+}
